cd8b1bc0218eb11d17d75ab4b2a1fd505953be40
spot <spot@vinova.com.sg>
2025-04-01 22:16:34 +0700

    Rubocop - Clean code

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, "git@github.com:spottran2001/shortening.git"
 set :ssh_options, { :forward_agent => true }
 
 set :pty, true
-set :linked_files, %w(config/database.yml config/credentials.yml.enc config/master.key)
+set :linked_files, %w(config/database.yml config/master.key)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 5
 set :rvm_type, :user
